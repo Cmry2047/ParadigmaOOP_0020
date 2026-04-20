df39b8b1fb40d5f65efa4da035418529bf1c611a
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-04-20 10:54:07 +0700

    Membuat Output NIM

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -7,4 +7,16 @@ class Mahasiswa{
         int nim;
         string nama;
         float nilai;
+        void printData(){
+            cout << "NIM : " << nim << endl;
+        }
 }; //batas Class
+
+int main(){
+    Mahasiswa mhs;
+    mhs.nim = 2025;
+    mhs.nama = "Fadhil";
+    mhs.nilai = 85.5;
+
+    mhs.printData();
+}
